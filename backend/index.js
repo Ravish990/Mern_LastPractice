61dd8c59b3c36dada97015c62f001cae862b8158
Ravish Kumar <ravish.kumar@chromium.org>
2025-05-04 23:18:19 +0530

    Hello

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,21 @@
 const express = require('express');
-const connectDb = require('./db/connection')
-
+const serverless = require('serverless-http');
+const connectDb = require('./db/connection');
 const productRoutes = require('./routes/productRoutes');
-const userRoutes = require('./routes/userRoutes')
+const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
-
-
-
 
 connectDb();
 
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT || 3000;
-
 app.use(express.json());
 
 app.get('/check', (req, res) => {
     res.send('Server is running!');
 });
 
-app.use('/', productRoutes)
+app.use('/', productRoutes);
+app.use('/user', userRoutes);
 
-app.use('/user',userRoutes)
-
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT} . http://localhost:3000/`);
-});
+module.exports = serverless(app);
